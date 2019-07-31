6a65e4e37a072ee395ff3ce20519d6bf9c6138fb
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-07-31 11:47:19 -0400

    remove post method from feeding schedules page
    it may be best to use the post method in `DeleteFeedingSchedulesServlet`
    in order to make use of its doGet method as well

--- src/com/examples/ezoo/servlets/FeedingSchedulesServlet.java
+++ src/com/examples/ezoo/servlets/FeedingSchedulesServlet.java
@@ -35,50 +35,5 @@ public class FeedingSchedulesServlet extends HttpServlet {
 				
 		request.getRequestDispatcher("feedingSchedules.jsp").forward(request, response);
 	}
-	
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Get Parameters
-		// We MUST convert to int since parameters are always Strings
 		
-		int scheduleID = Integer.parseInt(request.getParameter("scheduleID"));
-		
-		String feedingTime = request.getParameter("feedingTime");
-		String recurrence = request.getParameter("recurrence");
-		String food = request.getParameter("food");
-		String notes = request.getParameter("notes");
-		
-		// Create a FeedingSchedule object from the parameters
-		FeedingSchedule scheduleToDelete = new FeedingSchedule(
-				scheduleID,
-				feedingTime,
-				recurrence,
-				food,
-				notes);
-		
-		// Call DAO method
-		FeedingScheduleDAO dao = DAOUtilities.getFeedingScheduleDAO();
-		try {
-			dao.deleteFeedingSchedule(scheduleToDelete);
-			request.getSession().setAttribute("message",  "Feeding schedule successfully deleted");
-			request.getSession().setAttribute("messageClass", "alert-success");
-			response.sendRedirect("feedingSchedules");		// animal care servlet? "animalCare"
-		} catch(SQLIntegrityConstraintViolationException e) {
-			e.printStackTrace();
-			
-			// change the message
-			request.getSession().setAttribute("message",  "Id of " + scheduleToDelete.getScheduleID() + " does not exist");
-			request.getSession().setAttribute("messageClass",  "alert-danger");
-			request.getRequestDispatcher("feedingSchedules.jsp").forward(request, response);
-		} catch (Exception e) {
-			e.printStackTrace();
-			
-			// change the message
-			request.getSession().setAttribute("message",  "There was a problem deleting the feeding schedule at this time");
-			request.getSession().setAttribute("messageClass",  "alert-danger");
-			request.getRequestDispatcher("feedingSchedules.jsp").forward(request, response);
-		}
-		
-	}
-	
 }
