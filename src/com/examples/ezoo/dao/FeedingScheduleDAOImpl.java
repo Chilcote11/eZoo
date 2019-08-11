4b1aaa85b0b342dc8155a3699df922dfafdee577
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-08-10 20:05:15 -0400

    cleanup DAO
    351 lines to 94

--- src/com/examples/ezoo/dao/FeedingScheduleDAOImpl.java
+++ src/com/examples/ezoo/dao/FeedingScheduleDAOImpl.java
@@ -1,11 +1,5 @@
 package com.examples.ezoo.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.query.Query;
@@ -34,127 +28,18 @@ public class FeedingScheduleDAOImpl implements FeedingScheduleDAO{
 	@Override		// checked and tested
 	public void saveFeedingSchedule(FeedingSchedule feedingSchedule) throws Exception {
 		sessionFactory.getCurrentSession().save(feedingSchedule);
-		
-		/*Connection connection = null;
-		PreparedStatement stmt = null;
-		int success = 0;
-		
-		try {
-			connection = DAOUtilities.getConnection();
-			String sql = "INSERT INTO feeding_schedules VALUES (?, ?, ?, ?, ?)";
-			
-			// Setup PreparedStatement
-			stmt = connection.prepareStatement(sql);
-			
-			// Add parameters from feeding_schedules into PreparedStatement
-			stmt.setInt(1,  feedingSchedule.getScheduleID());
-			stmt.setString(2,  feedingSchedule.getFeedingTime());
-			stmt.setString(3,  feedingSchedule.getRecurrence());
-			stmt.setString(4,  feedingSchedule.getFood());
-			stmt.setString(5,  feedingSchedule.getNotes());
-			
-			success = stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				if (connection != null)
-					connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		if (success == 0) {
-			// then update didn't occur, throw an exception
-			throw new Exception("Save feeding schedule failed: " + feedingSchedule);
-		}*/
 	}
 
 	@Override
 	public void deleteFeedingSchedule(FeedingSchedule feedingSchedule) throws Exception {
 		sessionFactory.getCurrentSession().delete(feedingSchedule);
-		
-		/*Connection connection = null;
-		PreparedStatement stmt = null;
-		int success = 0;
-		
-		try {
-			connection = DAOUtilities.getConnection();
-			String sql = "DELETE FROM feeding_schedules WHERE schedule_id = ?";
-			
-			// Setup PreparedStatement
-			stmt = connection.prepareStatement(sql);
-			
-			// Add parameters from feeding_schedules into PreparedStatement
-			stmt.setInt(1,  feedingSchedule.getScheduleID());
-			
-			success = stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				if (connection != null)
-					connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		if (success == 0) {
-			throw new Exception("Delete feeding schedule failed: +" + feedingSchedule);
-		}*/
 	}
 
 	@Override // checked
 	public List<FeedingSchedule> getAllFeedingSchedules() {
-		Session session = sessionFactory.openSession();
+		Session session = sessionFactory.openSession();				// need to close? no warning..	
 		Query<FeedingSchedule> results = session.createQuery("from FeedingSchedule");
-		List<FeedingSchedule> feedingSchedules = results.list();		
-		
-		/*List<FeedingSchedule> feedingSchedules = new ArrayList<>();
-		Connection connection = null;
-		Statement stmt = null;
-
-		try {
-			connection = DAOUtilities.getConnection();
-
-			stmt = connection.createStatement();
-
-			String sql = "SELECT * FROM feeding_schedules";
-
-			ResultSet rs = stmt.executeQuery(sql);
-
-			while (rs.next()) {
-				FeedingSchedule fs = new FeedingSchedule();
-
-				fs.setScheduleID(rs.getInt("schedule_id"));
-				fs.setFeedingTime(rs.getString("feeding_time"));
-				fs.setRecurrence(rs.getString("recurrence"));
-				fs.setFood(rs.getString("food"));
-				fs.setNotes(rs.getString("notes"));
-								
-				feedingSchedules.add(fs);
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null) {
-					stmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}*/
+		List<FeedingSchedule> feedingSchedules = results.list();
 		return feedingSchedules;
 	}
 
@@ -164,53 +49,14 @@ public class FeedingScheduleDAOImpl implements FeedingScheduleDAO{
 		if (animal.getFeedingScheduleID() == null) {
 			return null;
 		}
-		
-		Session session = sessionFactory.openSession();		
+				
 		FeedingSchedule feedingSchedule = sessionFactory.getCurrentSession().get(FeedingSchedule.class, animal.getFeedingScheduleID());
 		
-		/*FeedingSchedule feedingSchedule = new FeedingSchedule();
-		Connection connection = null;
-		PreparedStatement stmt = null;
-		
-		try {
-			connection = DAOUtilities.getConnection();
-				
-			String sql = "SELECT * FROM feeding_schedules WHERE schedule_id = ?";
-			stmt = connection.prepareStatement(sql);
-			
-			stmt.setInt(1, animal.getFeedingScheduleID());
-
-			ResultSet rs = stmt.executeQuery();
-			
-			while (rs.next()) {
-				feedingSchedule.setScheduleID(rs.getInt("schedule_id"));
-				feedingSchedule.setFeedingTime(rs.getString("feeding_time"));
-				feedingSchedule.setRecurrence(rs.getString("recurrence"));
-				feedingSchedule.setFood(rs.getString("food"));
-				feedingSchedule.setNotes(rs.getString("notes"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null) {
-					stmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}*/
-
-		return feedingSchedule;
-		
+		return feedingSchedule;	
 	}
 
 	@Override
-	public void assignFeedingSchedule(FeedingSchedule feedingSchedule, Animal animal) throws Exception {
-		
+	public void assignFeedingSchedule(FeedingSchedule feedingSchedule, Animal animal) throws Exception {		
 		Session session = sessionFactory.openSession();	
 		session.beginTransaction();
 		Query query = session.createQuery("UPDATE Animal SET feedingScheduleID = :Schedule_ID WHERE animalID = :Animal_ID");
@@ -218,43 +64,10 @@ public class FeedingScheduleDAOImpl implements FeedingScheduleDAO{
 		query.setParameter("Animal_ID", animal.getAnimalID());
 		query.executeUpdate();
 		session.getTransaction().commit();
-		
-		/*Connection connection = null;
-		PreparedStatement stmt = null;
-		int success = 0;
-		
-		try {
-			connection = DAOUtilities.getConnection();
-			String sql = "UPDATE animals SET feeding_schedule = ? WHERE animalid = ?";
-			
-			// Setup PreparedStatement
-			stmt = connection.prepareStatement(sql);
-			
-			// Add parameters from animals into PreparedStatement
-			stmt.setInt(1,  feedingSchedule.getScheduleID());
-			stmt.setLong(2, animal.getAnimalID());
-			
-			success = stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				if (connection != null)
-					connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		if (success == 0)
-			throw new Exception("Assign feeding schedule failed: " + animal);*/
 	}
 
 	@Override
-	public void removeFeedingSchedule(Animal animal) throws Exception { 		// remove means set = null?
-		
+	public void removeFeedingSchedule(Animal animal) throws Exception { 		// remove means set = null?	
 		Session session = sessionFactory.openSession();	
 		session.beginTransaction();
 		Query query = session.createQuery("UPDATE Animal SET feedingScheduleID = :Schedule_ID WHERE animalID = :Animal_ID");
@@ -262,43 +75,10 @@ public class FeedingScheduleDAOImpl implements FeedingScheduleDAO{
 		query.setParameter("Animal_ID", animal.getAnimalID());
 		query.executeUpdate();
 		session.getTransaction().commit();
-		
-		/*Connection connection = null;
-		PreparedStatement stmt = null;
-		int success = 0;
-		
-		try {
-			connection = DAOUtilities.getConnection();
-			String sql = "UPDATE animals SET feeding_schedule = null WHERE animalid = ?";
-			
-			// Setup PreparedStatement
-			stmt = connection.prepareStatement(sql);
-			
-			// Add parameters from feeding_schedules into PreparedStatement
-			stmt.setLong(1,  animal.getAnimalID());
-			
-			success = stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				if (connection != null)
-					connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		if (success == 0) {
-			throw new Exception("Remove feeding schedule from animal failed: +" + animal);
-		}*/
 	}
 	
 	@Override
 	public void updateFeedingSchedule(FeedingSchedule feedingSchedule) throws Exception {
-		
 		Session session = sessionFactory.openSession();	
 		session.beginTransaction();
 		Query query = session.createQuery("UPDATE FeedingSchedule SET feedingTime = :FT, "
@@ -310,42 +90,5 @@ public class FeedingScheduleDAOImpl implements FeedingScheduleDAO{
 		query.setParameter("ID", feedingSchedule.getScheduleID());
 		query.executeUpdate();
 		session.getTransaction().commit();
-		
-		/*Connection connection = null;
-		PreparedStatement stmt = null;
-		int success = 0;
-		
-		try {
-			connection = DAOUtilities.getConnection();
-			String sql = "UPDATE feeding_schedules SET feeding_time = ?, recurrence = ?,"
-					+ " food = ?, notes = ? WHERE schedule_id = ?";
-			
-			// Setup PreparedStatement
-			stmt = connection.prepareStatement(sql);
-			
-			// Add parameters from animals into PreparedStatement
-			stmt.setString(1,  feedingSchedule.getFeedingTime());
-			stmt.setString(2,  feedingSchedule.getRecurrence());
-			stmt.setString(3,  feedingSchedule.getFood());
-			stmt.setString(4,  feedingSchedule.getNotes());
-			stmt.setInt(5,  feedingSchedule.getScheduleID());
-			
-			success = stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				if (connection != null)
-					connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		if (success == 0)
-			throw new Exception("Update feeding schedule failed: " + feedingSchedule);*/
 	}
-
 }
