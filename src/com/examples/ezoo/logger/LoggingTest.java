39628571fbf7f2971fdd151b8b021bde389adf0d
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-08-12 18:45:52 -0400

    attempt using PropertyConfigurator
    unsuccessful, same warnings

--- src/com/examples/ezoo/logger/LoggingTest.java
+++ src/com/examples/ezoo/logger/LoggingTest.java
@@ -2,12 +2,16 @@ package com.examples.ezoo.logger;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class LoggingTest {
 	
 	private static Logger Log = Logger.getLogger(LoggingTest.class.getName());
 
 	public static void main(String[] args) {
+		
+		String log4ConfPath = "C:/Software Stuff/Revature/eZoo/eZoo Files/src/log4j.properties";
+		PropertyConfigurator.configure(log4ConfPath);
 		
 		// The default logging level is DUBUG - this should be printed
 		Log.debug("Debug message");
