c29972d7c3e449538b38567c7b4b97f6d9387037
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-08-29 12:30:12 -0400

    add logging to HomeController

--- src/com/examples/ezoo/controllers/HomeController.java
+++ src/com/examples/ezoo/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.examples.ezoo.controllers;
 
+import org.apache.logging.log4j.Level;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.examples.ezoo.logger.Origin;
 import com.examples.ezoo.logger.ZooLogger;
 
 @Controller
@@ -13,6 +15,9 @@ public class HomeController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String displayHomePage() {
+		
+		Log.controllerLog(Origin.CONTROLLER_HOME_GET, Level.INFO, "navigation");
+		
 		return "index";
 	}
 }
