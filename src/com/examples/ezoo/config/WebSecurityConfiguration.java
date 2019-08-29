cedbf4d8988d979d5a99ba83b953d3eb67113fa2
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-08-28 20:53:38 -0400

    remove "/" from being ignored.
    still shows up.  adding HomeController fixed this

--- src/com/examples/ezoo/config/WebSecurityConfiguration.java
+++ src/com/examples/ezoo/config/WebSecurityConfiguration.java
@@ -33,7 +33,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/register");
-		web.ignoring().antMatchers("/");
+//		web.ignoring().antMatchers("/");
 	}
 	
 	@Autowired
