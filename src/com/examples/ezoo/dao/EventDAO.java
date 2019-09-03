482af7261f0e6613177e12fbe1d5879a3ff79744
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-09-03 16:39:08 -0400

    create EventDAO interface with methods for users and admins

---
+++ src/com/examples/ezoo/dao/EventDAO.java
@@ -0,0 +1,21 @@
+package com.examples.ezoo.dao;
+
+import java.util.List;
+
+import com.examples.ezoo.model.Event;
+import com.examples.ezoo.model.User;
+
+public interface EventDAO {
+	
+	// for users and admins
+	Event getEvent(Event event);									// check out an event
+	List<Event> getAllEvents();										// view zoo schedule
+	List<Event> getEventsByUser(User user);							// see events you're signed up for
+	void signUpForEvent(User user, Event event) throws Exception;	// sign up for event
+	void leaveEvent(User user, Event event) throws Exception;		// leave event
+
+	// for admins only
+	void saveEvent(Event event) throws Exception;					// create new event
+	void deleteEvent(Event event) throws Exception;					// delete an event
+	void updateEvent(Event event) throws Exception;					// update an event
+}
