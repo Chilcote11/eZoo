a276f8c7374c2d12ff3fbbafe83fe43a9d8e03c3
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-09-04 12:36:30 -0400

    create calendar controller and jsp for viewing all events
    forms and model objects created for deleting and updating events

---
+++ src/com/examples/ezoo/controllers/CalendarController.java
@@ -0,0 +1,47 @@
+package com.examples.ezoo.controllers;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.examples.ezoo.config.Config;
+import com.examples.ezoo.dao.EventDAO;
+import com.examples.ezoo.logger.ZooLogger;
+import com.examples.ezoo.model.Event;
+
+@Controller
+public class CalendarController {
+
+	private ZooLogger Log = new ZooLogger();
+	
+	@RequestMapping(value="/calendar", method=RequestMethod.GET)
+	public String DisplayCalendar(Model model
+			, @ModelAttribute("message") String message
+			, @ModelAttribute("messageClass") String messageClass) {
+		
+		// TODO logging
+		
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+		EventDAO dao = context.getBean(EventDAO.class);
+		List<Event> events = dao.getAllEvents();
+		Collections.sort(events);
+		
+		// Pupulate the list into a variable that will be stored in the model
+		model.addAttribute("events", events);
+		
+		// 
+		model.addAttribute("eventToDelete", new Event());
+		
+		model.addAttribute("eventToUpdate", new Event());
+		
+		context.close();
+		return "calendar";
+	}
+}
